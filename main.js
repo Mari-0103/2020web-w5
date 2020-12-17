2b428150fa01a6508699f787e842ac1e2b8bfb95
Mari-0103 <75403363+Mari-0103@users.noreply.github.com>
2020-12-17 13:47:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let insertX = ['ミッキー','ドナルド','デイジー'];
+let insertY = ['愛媛','福岡','和歌山'];
+let insertZ = ['焼けた','溶けた','這った'];
 
 randomize.addEventListener('click', result);
 
