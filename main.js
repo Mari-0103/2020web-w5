2fd6159ccfbc9e156c1b9f2d983e57d1edd5c1ed
Mari-0103 <75403363+Mari-0103@users.noreply.github.com>
2020-12-17 13:35:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '気温は華氏14度で外は寒かった。 :insertx: は外に出かけた。:inserty:　につくと、混雑していた。体重300ポンドの :insertx:　は驚いた。:inserty:　ではよくあることだった。';
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['ミッキーマウス','ドナルド','ダッフィー'];
 let insertY = ['愛媛','福岡','大阪'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
@@ -18,23 +19,26 @@ function result() {
 
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('夏',name);
+    newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("日本").checked) {
+  if(document.getElementById("uk").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((14-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('14 fahrenheit',temperature);
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature);
     newStory = newStory.replace('300 pounds',weight);
   }
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
